56c115e4cbd2210ef08fbab7133354e7f4608018
HHB <bcl0206@gmail.com>
2023-09-12 00:05:56 +0900

    feat: 버튼 리스트 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{useState,MouseEvent} from 'react';
 import './App.css';
 import CombinationChart from './components/charts/CombinationChart';
+import CategoryBtnList from './components/charts/CategoryBtnList';
 
 function App() {
+
+  const [selected, setSelected] = useState<string>('해제');
+  const selectCategory = (e:MouseEvent<HTMLButtonElement>) => {
+  setSelected(e.currentTarget.innerText);
+}  
+
   return (
-    <div className="App">
-    <CombinationChart/>
-    </div>
+    <>
+    <CategoryBtnList selectCategory={selectCategory} selected={selected} />
+    <CombinationChart setSelected={setSelected} selected={selected}/>
+    </>
   );
 }
 
